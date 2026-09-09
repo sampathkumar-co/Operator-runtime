5382c9a6769b90d1bd94e907c7ef2376db6e10d6
sampathkumar-co <sk96429643@gmail.com>
2026-09-09 20:40:57 +0530

    feat: export VS Code adapter

--- src/capabilities/index.ts
+++ src/capabilities/index.ts
@@ -10,6 +10,7 @@ export * from './project-command.ts';
 export * from './project-transaction.ts';
 export * from './docker.ts';
 export * from './postgres.ts';
+export * from './vscode.ts';
 export * from './browser-cdp.ts';
 export * from './browser-managed.ts';
 export * from './windows-uia.ts';
